9e9c02061b7f94e8711b729408ed73cccd628ceb
Rob Brooks <robertbrooks334@gmail.com>
2016-11-17 16:23:18 +0000

    updated mongo url

--- bin/server.js
+++ bin/server.js
@@ -57,8 +57,11 @@ var mongoose = require('mongoose');
 // Use native Node promises
 mongoose.Promise = global.Promise;
 
+//var connecturl = process.env.URL;
+
 // connect to MongoDB
-mongoose.connect('mongodb://localhost/todo-api')
+//mongoose.connect(process.env.URL)
+mongoose.connect('mongodb://rob:rob@ds157247.mlab.com:57247/dwp-rota')
   .then(() =>  console.log('connection succesful'))
   .catch((err) => console.error(err));
 
